e519e74be2d99db1787d240ab2857d980cb6d453
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-01-19 16:57:11 -0300

    feat: add eslint and prettier

--- src/commands/node-crud-typeorm.ts
+++ src/commands/node-crud-typeorm.ts
@@ -1,12 +1,17 @@
 import { GluegunCommand } from 'gluegun'
 
 const command: GluegunCommand = {
-  name: 'node-crud-typeorm',
-  run: async toolbox => {
-    const { print } = toolbox
+    name: 'node-crud-typeorm',
+    run: async toolbox => {
+        const { print, parameters } = toolbox
+        const { options } = parameters
 
-    print.info('Welcome to your CLI')
-  }
+        const strings = options.strings as string
+
+        console.log(parameters)
+
+        print.info('Welcome to your CLI')
+    }
 }
 
 module.exports = command
